7cdd31bf72cdf72608c8efe23d49706a6c9eaf9d
unknown <beeenaya98@gmail.com>
2022-05-31 10:41:17 +0545

    Vb_codes

---
+++ VariableDeclaration.vb
@@ -0,0 +1,20 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim a As Integer = 1010
+        Dim b As Double = 202.2
+        Dim c As Long=1010101001
+        Dim d As Short = 10101
+        Dim e As String = "Binaya"
+
+        Console.WriteLine(" Integer of 4 bytes and 32 bits " & a)
+        Console.WriteLine(" Double of 8 bytes and 64 bits " & b)
+        Console.WriteLine(" Long Integer of 8 bytes and 64 bits " & c)
+        Console.WriteLine(" Short Integer of 2 bytes and 16 bits " & d)
+        Console.WriteLine(" String " & e)
+
+
+
+    End Sub
+End Module
